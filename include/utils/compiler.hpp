22f9cc6ed143abb943a10e8511bd2f6246fceff0
Данил Голдышев <goldyshev02@mail.ru>
2025-04-01 17:41:09 +0300

    Added UNREACHABLE macro

---
+++ include/utils/compiler.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdlib>
+#include "logger.hpp"
+
+#if defined(__GNUC__) || defined(__clang__)
+#define OCTET_BUILTIN_UNREACHABLE() __builtin_unreachable()
+#elif defined(_MSC_VER)
+#define OCTET_BUILTIN_UNREACHABLE() __assume(false)
+#else
+#define OCTET_BUILTIN_UNREACHABLE() ((void)0)
+#endif
+
+// Макрос для недостижимых веток кода
+#define UNREACHABLE(reason)                                                                        \
+    do {                                                                                           \
+        LOG_CRITICAL << "UNREACHABLE code reached: " << reason;                                    \
+        std::abort();                                                                              \
+        OCTET_BUILTIN_UNREACHABLE();                                                               \
+    } while (0)
